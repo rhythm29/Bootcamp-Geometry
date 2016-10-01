d7bfa841250d9de7ea2e2c7d1939914428a41595
rhythmwaliamodi <rhythmwalia29@gmail.com>
2016-10-01 16:51:04 +0530

    [REFACTOR] #polygon:perimeter method

--- lib/geometry/polygon.rb
+++ lib/geometry/polygon.rb
@@ -1,16 +1,14 @@
 class Geometry::Polygon
 
-  def initialize(side1, side2, side3, side4, side5, side6)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-    @side4 = side4
-    @side5 = side5
-    @side6 = side6
+  def initialize(polygon)
+    @polygon = polygon
   end
 
   def perimeter
-    @side1+@side2+@side3+@side4+@side5+@side6
+
+  sum = 0
+  @polygon.each { |a| sum+=a }
+  return sum
   end
   
 end
--- spec/geometry/polygon_spec.rb
+++ spec/geometry/polygon_spec.rb
@@ -4,12 +4,12 @@ describe Geometry::Polygon do
   describe "#polygon" do
 
     it "should return 0 when all the sides are 0" do
-      polygon = Geometry::Polygon.new(0,0,0,0,0,0)
+      polygon = Geometry::Polygon.new([0])
       expect(polygon.perimeter).to eq(0)
     end
 
     it "should return 21 when given sides are 1,2,3,4,5,6" do
-      polygon = Geometry::Polygon.new(1,2,3,4,5,6)
+      polygon = Geometry::Polygon.new([1,2,3,4,5,6])
       expect(polygon.perimeter).to eq(21)
     end
   end
