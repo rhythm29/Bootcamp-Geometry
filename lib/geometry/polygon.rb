d97ab8844fec9afb8f4c4caf5988518fc761c552
rhythmwaliamodi <rhythmwalia29@gmail.com>
2016-10-01 16:43:12 +0530

    Polygon::perimeter add spec to calculate perimeter

--- lib/geometry/polygon.rb
+++ lib/geometry/polygon.rb
@@ -2,15 +2,15 @@ class Geometry::Polygon
 
   def initialize(side1, side2, side3, side4, side5, side6)
     @side1 = side1
-    @side2 = side1
-    @side3 = side1
-    @side4 = side1
-    @side5 = side1
-    @side6 = side1
+    @side2 = side2
+    @side3 = side3
+    @side4 = side4
+    @side5 = side5
+    @side6 = side6
   end
 
   def perimeter
-    0
+    @side1+@side2+@side3+@side4+@side5+@side6
   end
   
 end
--- spec/geometry/polygon_spec.rb
+++ spec/geometry/polygon_spec.rb
@@ -7,5 +7,10 @@ describe Geometry::Polygon do
       polygon = Geometry::Polygon.new(0,0,0,0,0,0)
       expect(polygon.perimeter).to eq(0)
     end
+
+    it "should return 21 when given sides are 1,2,3,4,5,6" do
+      polygon = Geometry::Polygon.new(1,2,3,4,5,6)
+      expect(polygon.perimeter).to eq(21)
+    end
   end
 end
